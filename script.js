5b48f6b1d38f30e32e919c99eea993712a9179c8
Lauren Suna <laurensuna@gmail.com>
2020-04-08 09:55:15 -0400

    project files

---
+++ script.js
@@ -0,0 +1,29 @@
+//I tried lol
+//$(document).ready(function () {
+//
+//    var color = "red";
+//
+//    if (color = "red") {
+//        $(".btn").click(function () {
+//            $("#bg").css("background-color", "yellow");
+//        });
+//        var color = "yellow"
+//        console.log(color);
+//    }
+//    
+//    if (color = "yellow"){
+//        $(".btn").click(function() {
+//            $("#bg").css("background-color", "red")
+//        });
+//        var color = "red";
+//        console.log(color);
+//    }
+//
+//});
+
+
+$(document).ready(function(){
+  $(".btn").click(function(){
+    $("#bg").css("background-color", "#C9DAEA");
+  });
+});
